fcae229bc2a4dd39079d2a0b1b843a18f6de6e1c
MARIO NISHIO <mario.nishio@gmail.com>
2018-04-03 15:49:44 -0700

    Fixed a NULL issue when user's left no input for questions, font, font color, added

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@
 
 alert('Welcome to Mario\'s About Me page!');
 
-var myEthnicity = prompt('I first have to ask you, did you find it surprising that my name is MARIO even though I\'m Asian American? Answer y or n.').toLowerCase();
-if(myEthnicity === 'y') {
+var myEthnicity = prompt('I first have to ask you, did you find it surprising that my name is MARIO even though I\'m Asian American? Answer y or n.');
+if(myEthnicity === null) {
+  alert('I guess, I won\'t know if you were surprised');
+  console.log('User did not make an input');
+} else if(myEthnicity.toLowerCase() === 'y') {
   alert('Haha, it\'s different right? My parents wanted a name that could be used in both Japan and US!');
   console.log('User was surprised by my name.');
-} else if(myEthnicity === 'n') {
+} else if(myEthnicity.toLowerCase() === 'n') {
   alert('Oh really? Did you meet another Asian with the name Mario. You have to introduce me to them sometime!');
   console.log('User was not surprised by my name.');
 } else {
@@ -19,11 +22,14 @@ alert('Let\'s now play a game! I\'m going to ask you a series of question about 
 var correctAns = 'Yep, you\'re right! :)';
 var wrongAns = 'Sorry, your\'re wrong :(';
 
-var favSport = prompt('Do you think my favorite sport is soccer?').toLowerCase();
-if(favSport === 'yes') {
+var favSport = prompt('Do you think my favorite sport is soccer?');
+if (favSport === null) {
+  alert('You didn\'t say anything!');
+  console.log('User did not make an input');
+}else if(favSport.toLowerCase() === 'yes') {
   alert(wrongAns);
   console.log('User thinks I like soccer');
-} else if(favSport === 'no') {
+} else if(favSport.toLowerCase() === 'no') {
   alert(correctAns);
   console.log('User thinks I do not like soccer');
 } else {
@@ -32,24 +38,30 @@ if(favSport === 'yes') {
 }
 alert('Soccer isn\'t my favorite sport. My favorite is actually Ultimate Frisbee. I also like a bunch of other sports like Tennis, ping pong, and e-sports haha.');
 
-var favPets = prompt('My favorite pets are dogs! Do you think that statement is true?').toLocaleLowerCase();
-if(favPets === 'yes') {
+var favPets = prompt('My favorite pets are dogs! Do you think that statement is true?');
+if (favPets === null) {
+  alert('You didn\'t say anything!');
+  console.log('User did not make an input');
+}else if(favPets.toLowerCase() === 'yes') {
   alert(correctAns);
   console.log('User thinks I like dogs');
-} else if(favPets === 'no') {
+} else if(favPets.toLowerCase() === 'no') {
   alert(wrongAns);
   console.log('User thinks I do not like dogs');
 } else {
   alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
   console.log('User did not follow the rules');
 }
-alert('In fact, I have two dogs! A shibe named Luna and half-shibe half-husky named Noctis!');
+alert(' I love dogs; in fact, I have two dogs! A shibe named Luna and half-shibe half-husky named Noctis!');
 
-var favKaraoke = prompt('Do you think I like karaoke?').toLocaleLowerCase();
-if(favKaraoke === 'yes') {
+var favKaraoke = prompt('Do you think I like karaoke?');
+if (favKaraoke === null) {
+  alert('You didn\'t say anything!');
+  console.log('User did not make an input');
+}else if(favKaraoke.toLowerCase() === 'yes') {
   alert(correctAns);
   console.log('User thinks I like karaoke');
-} else if(favKaraoke === 'no') {
+} else if(favKaraoke.toLowerCase() === 'no') {
   alert(wrongAns);
   console.log('User thinks I do not karaoke');
 } else {
@@ -58,13 +70,16 @@ if(favKaraoke === 'yes') {
 }
 alert('Karaoke is one of my favorite things to do. It\'s just unfortunate that I\'m pretty toned-death. It\'s all good if you are having fun right? XP');
 
-alert('Now for some Lighting Round. Remember to answer with "yes or "no".');
+// alert('Now for some Lighting Round. Remember to answer with "yes or "no".');
 
-var favColor = prompt('My favoirte font color is Red? Is this true about me?').toLocaleLowerCase();
-if(favColor === 'yes') {
+var favColor = prompt('My favoirte font color is Red? Is this true about me?');
+if (favColor === null) {
+  alert('You didn\'t say anything!');
+  console.log('User did not make an input');
+}else if(favColor.toLocaleLowerCase() === 'yes') {
   alert(correctAns);
   console.log('User thinks I like red');
-} else if(favColor === 'no') {
+} else if(favColor.toLocaleLowerCase() === 'no') {
   alert(wrongAns);
   console.log('User thinks I do not like red');
 } else {
@@ -72,38 +87,38 @@ if(favColor === 'yes') {
   console.log('User did not follow the rules');
 }
 
-var favShape = prompt('My favorite shape is Rectangles. Do you think that\'s true about me?').toLocaleLowerCase();
-if(favShape === 'yes') {
-  alert(correctAns);
-  console.log('User thinks I like rectangles');
-} else if(favShape === 'no') {
-  alert(wrongAns);
-  console.log('User thinks I do not like rectangles');
-} else {
-  alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
-  console.log('User did not follow the rules');
-}
+// var favShape = prompt('My favorite shape is Rectangles. Do you think that\'s true about me?').toLocaleLowerCase();
+// if(favShape === 'yes') {
+//   alert(correctAns);
+//   console.log('User thinks I like rectangles');
+// } else if(favShape === 'no') {
+//   alert(wrongAns);
+//   console.log('User thinks I do not like rectangles');
+// } else {
+//   alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
+//   console.log('User did not follow the rules');
+// }
 
-var favFont = prompt('My favorite font is Futura? Is this true about me?').toLocaleLowerCase();
-if(favFont === 'yes') {
-  alert(correctAns);
-  console.log('User thinks I like Futura');
-} else if(favFont === 'no') {
-  alert(wrongAns);
-  console.log('User thinks I do not like Futura');
-} else {
-  alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
-  console.log('User did not follow the rules');
-}
+// var favFont = prompt('My favorite font is Futura? Is this true about me?').toLocaleLowerCase();
+// if(favFont === 'yes') {
+//   alert(correctAns);
+//   console.log('User thinks I like Futura');
+// } else if(favFont === 'no') {
+//   alert(wrongAns);
+//   console.log('User thinks I do not like Futura');
+// } else {
+//   alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
+//   console.log('User did not follow the rules');
+// }
 
-var favWord = prompt('My favorite word is "Supreme"? Is this true?').toLocaleLowerCase();
-if(favWord === 'yes') {
-  alert(correctAns);
-  console.log('User thinks I like the word "Supreme"');
-} else if(favWord === 'no') {
-  alert(wrongAns);
-  console.log('User thinks I do not like the word "Supreme"');
-} else {
-  alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
-  console.log('User did not follow the rules');
-}
+// var favWord = prompt('My favorite word is "Supreme"? Is this true?').toLocaleLowerCase();
+// if(favWord === 'yes') {
+//   alert(correctAns);
+//   console.log('User thinks I like the word "Supreme"');
+// } else if(favWord === 'no') {
+//   alert(wrongAns);
+//   console.log('User thinks I do not like the word "Supreme"');
+// } else {
+//   alert('Hey! Follow the rules of the game! Please answer "yes" or "no".');
+//   console.log('User did not follow the rules');
+// }
